485d883eb796c8863784b82edb7d171cd3e3b48c
frank <frank@DESKTOP-O19LBSU>
2022-08-28 07:03:08 -0700

    Library samples

---
+++ AVEVAInsight.APIClient.Samples/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using AVEVAInsight.APIClient.CsvJsonUploader;
+global using AVEVAInsight.APIClient.EngineeringUnits;
+global using AVEVAInsight.APIClient.EventsData;
+global using AVEVAInsight.APIClient.Models;
+global using AVEVAInsight.APIClient.ProcessData;
+global using AVEVAInsight.APIClient.Solution;
+global using AVEVAInsight.APIClient.Tags;
